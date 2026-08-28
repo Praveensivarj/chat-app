c8692031f4ccea8c5880ac4400ec812555a01cfd
Praveenkumar <praveenkumar@codegama.com>
2026-08-28 12:45:54 +0530

    chore: ignore dist folder and remove tracked build artifacts

--- dist/src/routes/lookups.routes.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = require("express");
-const lookups_controller_1 = require("../controllers/lookups.controller");
-const router = (0, express_1.Router)();
-router.get('/mobile-country-codes', lookups_controller_1.mobileCountryCodes);
-router.get('/countries', lookups_controller_1.countries);
-exports.default = router;
